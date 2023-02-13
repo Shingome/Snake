35195cba9b4ed3703900c08c43f1664f77ed6d69
Shingome <mishakkoles@gmail.com>
2023-02-13 16:19:37 +0300

    create snake snake movement

--- snake.py
+++ snake.py
@@ -1,48 +1,88 @@
 import math
-
 import pygame
 from pygame.color import THECOLORS
 
 
+class Snake:
+    def __init__(self):
+        first_pos = tuple(frame[i] + cell_size * (cells[i] // 2) for i in range(2))
+        self.head_size = cell_size
+        self.color = THECOLORS['purple']
+        self.head = pygame.draw.rect(screen, self.color,
+                                     pygame.Rect(first_pos[0], first_pos[1], self.head_size, self.head_size))
+        self.direction = "UP"
+        self.directions = {"UP": (0, -cell_size), "DOWN": (0, cell_size),
+                           "RIGHT": (cell_size, 0), "LEFT": (-cell_size, 0)}
+        self.even = bool((cells[0] // 2) % 2)
+
+    def change_direction(self, direction: str):
+        if self.directions[direction][0] == -self.directions[self.direction][0] \
+                and self.directions[direction][1] == -self.directions[self.direction][1]:
+            return
+        self.direction = direction
+
+    def move(self):
+        # Change previous cell
+        color = cell_colors[0] if self.even else cell_colors[1]
+        pygame.draw.rect(screen, color,
+                         pygame.Rect(self.head.x, self.head.y, self.head_size, self.head_size))
+        self.even = not self.even
+
+        # Move head
+        self.head.move_ip(self.directions[self.direction])
+
+        # Borders
+        if self.head.x <= frame[0] - cell_size:
+            self.head.x = frame[2] - cell_size
+        elif self.head.x >= frame[2]:
+            self.head.x = frame[0]
+        elif self.head.y <= frame[1] - cell_size:
+            self.head.y = frame[3] - cell_size
+        elif self.head.y >= frame[3]:
+            self.head.y = frame[1]
+
+        pygame.draw.rect(screen, self.color, snake.head, 0)
+
+
 def find_size(window_size: (int, int), rel: float, cells: (int, int)):
-    border = int(window_size[1] * rel)
+    border = math.floor(window_size[1] * rel)
     cell_size = math.floor((window_size[1] - border) / cells[1])
     frame_size = tuple(i * cell_size for i in cells)
-    print(frame_size)
-    border = tuple(int((window_size[i] - frame_size[i]) / 2) for i in range(2))
+    border = tuple((window_size[i] - frame_size[i]) // 2 for i in range(2))
     start = tuple(window_size[i] - frame_size[i] - border[i] for i in range(2))
     end = tuple(window_size[i] - border[i] for i in range(2))
     return cell_size, *start, *end
 
 
 if __name__ == "__main__":
-    FPS = 30
+    FPS = 5
     size = (1200, 800)
     cells = (18, 12)
+    cell_colors = ((171, 216, 80), (163, 210, 71))
 
     # Main screen
     pygame.init()
     screen = pygame.display.set_mode(size)
     screen.fill(THECOLORS['blue'])
+    pygame.display.set_caption("Snake")
+    clock = pygame.time.Clock()
 
     # Game screen
     cell_size, *frame = find_size(size, 0.05, cells)
-    pygame.draw.rect(screen, (171, 216, 80), pygame.Rect(frame[0], frame[1], frame[2] - frame[0], frame[3] - frame[1]))
+    pygame.draw.rect(screen, cell_colors[0], pygame.Rect(frame[0], frame[1], frame[2] - frame[0], frame[3] - frame[1]))
 
     # Grid
     even = True
     x_start, y_start, x_end, y_end = frame
     for y in range(y_start, y_end, cell_size):
         for x in range(x_start if even else x_start + cell_size, x_end, cell_size * 2):
-            print(x, y)
-            pygame.draw.rect(screen, (163, 210, 71), pygame.Rect(x, y, cell_size, cell_size))
+            pygame.draw.rect(screen, cell_colors[1], pygame.Rect(x, y, cell_size, cell_size))
         even = not even
 
-    pygame.display.set_caption("Snake")
-    clock = pygame.time.Clock()
-
+    # Init snake
+    snake = Snake()
+    # Run
     pygame.display.flip()
-
     running = True
     while running:
         clock.tick(FPS)
@@ -50,3 +90,17 @@ if __name__ == "__main__":
             # check for closing window
             if event.type == pygame.QUIT:
                 running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    snake.change_direction("LEFT")
+                elif event.key == pygame.K_RIGHT:
+                    snake.change_direction("RIGHT")
+                elif event.key == pygame.K_UP:
+                    snake.change_direction("UP")
+                elif event.key == pygame.K_DOWN:
+                    snake.change_direction("DOWN")
+
+        snake.move()
+
+        pygame.display.update()
